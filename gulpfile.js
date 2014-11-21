468cb0be487de78c4ca85ef4353502c4915e8d20
Cesar Augusto D. Azevedo <cesardeazevedo@outlook.com>
2014-11-21 10:19:38 -0200

    release gulpfile with browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,38 @@
-var gulp = require('gulp'),
-  nodemon = require('gulp-nodemon'),
-  livereload = require('gulp-livereload'),
-  sass = require('gulp-ruby-sass');
+var gulp    = require('gulp'),
+    nodemon = require('gulp-nodemon'),
+    harp    = require('harp'),
+    browserSync = require('browser-sync');
 
-gulp.task('sass', function () {
-  return gulp.src('./public/css/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./public/css'))
-    .pipe(livereload());
+gulp.task('browser-sync', ['nodemon'], function () {
+    browserSync.init(null, {
+        proxy: "localhost:3000",
+        port: 9000,
+        open: false,
+    });
+});
+
+gulp.task('nodemon', function(){
+    nodemon({
+        script: 'app.js',
+        ext: 'js jade sass',
+    })
+    .on('restart', function () {
+        setTimeout(function () {
+            browserSync.reload({stream: true});
+        }, 500);
+    });
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./public/css/*.scss', ['sass']);
+
+    //Watch jade files
+    gulp.watch('app/views/**/*.jade', function(){
+        browserSync.reload('index.html', { stream: true });
+    });
+    //Watch sass files
+    gulp.watch('public/css/*.sass', function(){
+        browserSync.reload('bitvagas.css', { stream: true });
+    });
 });
 
-gulp.task('develop', function () {
-  livereload.listen();
-  nodemon({
-    script: 'app.js',
-    ext: 'js jade',
-  }).on('restart', function () {
-    setTimeout(function () {
-      livereload.changed();
-    }, 500);
-  });
-});
-
-gulp.task('default', [
-  'sass',
-  'develop',
-  'watch'
-]);
+gulp.task('default', ['browser-sync', 'watch']);
