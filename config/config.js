8438c3aaf272a37cc3aacdd842cc6933eb9f0154
César Augusto D. Azevedo <cesardeazevedo@outlook.com>
2014-12-23 02:18:05 -0500

    added user and passoword envrionment variables and comma-first

--- config/config.js
+++ config/config.js
@@ -1,41 +1,41 @@
-var path = require('path'),
-    rootPath = path.normalize(__dirname + '/..'),
-    env = process.env.NODE_ENV || 'development';
+var path     = require('path')
+  , rootPath = path.normalize(__dirname + '/..')
+  , env      = process.env.NODE_ENV || 'development'
+  , user     = process.env.user     || 'postgres'
+  , password = process.env.password || '';
 
 var config = {
   development: {
-    root: rootPath,
-    app: {
-      name: 'bitvagas-com'
-    },
-    port: 3000,
-    username: 'root',
-    password: '',
-    db: 'BITVAGAS'
+    root: rootPath
+    , app: {
+        name: 'bitvagas'
+    }
+    , port: 3000
+    , username: user
+    , password: password
+    , db: 'bitvagas'
   },
 
   test: {
-    root: rootPath,
-    app: {
-      name: 'bitvagas-com'
-    },
-    port: 3000,
-    username: 'root',
-    password: '',
-    db: 'BITVAGAS'
-    
+    root: rootPath
+    , app: {
+        name: 'bitvagas'
+    }
+    , port: 3000
+    , username: user
+    , password: password
+    , db: 'bitvagas'
   },
 
   production: {
-    root: rootPath,
-    app: {
-      name: 'bitvagas-com'
-    },
-    port: 3000,
-    username: 'root',
-    password: '',
-    db: 'BITVAGAS'
-    
+    root: rootPath
+    , app: {
+        name: 'bitvagas-com'
+    }
+    , port: 3000
+    , username: user
+    , password: password
+    , db: 'bitvagas'
   }
 };
 
