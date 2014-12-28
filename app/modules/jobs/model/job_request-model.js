6e36df92d7c836024263ee6842232c58ebfa35f9
César Augusto D. Azevedo <cesardeazevedo@outlook.com>
2014-12-28 05:49:59 -0500

    implemented job requests model

---
+++ app/modules/jobs/model/job_request-model.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes){
+
+    var JOB_REQUEST = sequelize.define('job_request', {
+          DATE   : DataTypes.DATE
+        , EMAIL  : {
+            type : DataTypes.STRING
+          , allowNull : true
+          , validate  : {
+            isEmail   : true
+          }
+        }
+        , DESCRIPTION : DataTypes.TEXT
+    }, {
+        classMethods  : {
+            associate : function(models){
+                JOB_REQUEST.belongsTo(models.job);
+                JOB_REQUEST.belongsTo(models.user);
+            }
+      }
+    });
+
+    return JOB_REQUEST;
+};
