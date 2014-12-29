d9de36eea38ae46f19c5bd0e4a56d283e879ead8
César Augusto D. Azevedo <cesardeazevedo@outlook.com>
2014-12-29 05:41:37 -0500

    added unit test for create users

--- app/modules/users/test/user-test.js
+++ app/modules/users/test/user-test.js
@@ -1,22 +1,36 @@
 var assert    = require('assert')
   , should    = require('should')
-  , Sequelize = require('sequelize')
   , db        = require('../../../models');
 
 describe('Users modules', function(){
+
     it('find all users', function(done){
-        db.USER.findAll().success(function(users){
+        db.user.findAll().success(function(users){
             console.log(JSON.stringify(users));
             users.should.be.ok;
             done();
         })
     })
-    it('find user by email `cesardeazevedo@outlook.com` ', function(done){
-        db.USER.find({ where: { EMAIL: 'cesardeazevedo@outlook.com'}})
+    it('create an user with email `test@bitvagas`', function(done){
+        db.user.create({ NAME: 'User Test'
+                       , EMAIL: 'test@bitvagas.com'
+                       , PASSWORD: 'password'
+                       , ADMIN: false
+        }).success(function(user){
+            console.log(JSON.stringify(user));
+            user.should.be.ok;
+            done();
+        }).error(function(error){
+            console.log('error: '+error);
+            done();
+        })
+    })
+    it('find user by email `test@bitvagas.com` ', function(done){
+        db.user.find({ where: { EMAIL: 'test@bitvagas.com'}})
         .success(function(users){
             console.log(JSON.stringify(users));
             users.should.be.ok;
             done();
-        });
-    });
-});
+        })
+    })
+})
