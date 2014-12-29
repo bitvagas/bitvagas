21d380fde51a2367e5f393556c2c70395e9586bd
César Augusto D. Azevedo <cesardeazevedo@outlook.com>
2014-12-29 04:27:34 -0500

    refactor unit test

--- test/bitvagas-test.js
+++ test/bitvagas-test.js
@@ -1,19 +1,23 @@
 var assert    = require('assert')
-  , Sequelize = require('sequelize');
-
-var sequelize = new Sequelize('bitvagas'
-                            , process.env.user
-                            , process.env.password,{
-    dialect: 'postgres'
-  , host:    'localhost'
-  , port:    5433
-});
+  , config    = require('../config/config')
+  , db        = require('../app/models');
 
 describe('connection', function(){
     it("connected to database", function(done){
-        sequelize.authenticate().complete(function(err){
+        db.sequelize.authenticate().complete(function(err){
+
+            console.log(err ? 'Unable to connect to the database: ' + err :
+                               'Connected has ben established succesfully');
             assert.equal(err, null);
             done();
         });
-    });
+    })
+    it("sync database", function(done){
+        db.sequelize.sync({ force : true })
+        .complete(function(err) {
+            console.log(err ? 'error: '+err : 'Database Synchronized');
+            assert.equal(err, null);
+            done();
+        })
+    })
 });
