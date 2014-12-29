3ba90ea73129e2f118aa7bc3b183b7fb599710e2
César Augusto D. Azevedo <cesardeazevedo@outlook.com>
2014-12-29 05:42:42 -0500

    remove variable

--- app/modules/jobs/test/jobs-test.js
+++ app/modules/jobs/test/jobs-test.js
@@ -1,6 +1,5 @@
 var assert    = require('assert')
   , should    = require('should')
-  , Sequelize = require('sequelize')
   , db        = require('../../../models');
 
 describe('Jobs modules api', function(){
