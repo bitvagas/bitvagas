b93988f9b8f57c81be14a62599ad7285f108b99d
César Augusto D. Azevedo <cesardeazevedo@outlook.com>
2015-01-01 19:30:40 -0500

    added job-service

---
+++ public/modules/jobs/services/job-service.js
@@ -0,0 +1,11 @@
+angular.module('bitvagas.jobs.services', [])
+.service('JobService', JobService);
+
+JobService.$inject = ['$http'];
+function JobService($http){
+    var baseUrl = '/api/jobs/';
+
+    this.create = function(job){
+        return $http.post(baseUrl + 'create', job);
+    };
+}
