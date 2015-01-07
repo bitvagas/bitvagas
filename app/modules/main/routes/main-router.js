cbee877eaf51dd2d242c173a1d3a9c9393e92e73
César Augusto D. Azevedo <cesardeazevedo@outlook.com>
2015-01-07 02:43:08 -0500

    pass user informations to views and logout route

--- app/modules/main/routes/main-router.js
+++ app/modules/main/routes/main-router.js
@@ -1,9 +1,13 @@
 var express = require('express')
   , router  = express.Router();
 
+router.get('/', function(request, response){
+    response.render("index", { user : request.user });
+});
 
-router.get('/', function(request,response){
-    response.render("index");
+router.get('/logout', function(request, response){
+    request.logout();
+    response.redirect('/');
 });
 
 module.exports = router;
