944d1d7f89460382c39d31a2d9091921df2e7670
César Augusto D. Azevedo <cesardeazevedo@outlook.com>
2015-01-15 22:02:46 -0500

    added index state

--- public/modules/main/app.js
+++ public/modules/main/app.js
@@ -3,16 +3,20 @@ angular.module('bitvagas.main', ['ui.router'])
 
         $urlRouterProvider.otherwise('/');
         $stateProvider
+        .state('index', {
+            url: '/'
+          , templateUrl: 'modules/jobs/views/job-list'
+        })
         .state('about', {
-            url: '/about',
-            templateUrl: 'modules/main/views/about'
+            url: '/about'
+          , templateUrl: 'modules/main/views/about'
         })
         .state('terms', {
-            url: '/terms',
-            templateUrl: 'modules/main/views/terms'
+            url: '/terms'
+          , templateUrl: 'modules/main/views/terms'
         })
         .state('contact', {
-            url: '/contact',
-            templateUrl: 'modules/main/views/contact'
+            url: '/contact'
+          , templateUrl: 'modules/main/views/contact'
         });
     });
