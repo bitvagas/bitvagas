167490faae36e4a62e64993441c5cc8fa85bebc9
Cesar Augusto D. Azevedo <cesardeazevedo@outlook.com>
2015-02-04 23:40:29 -0200

    added default value to active job field

--- app/modules/jobs/model/job-model.js
+++ app/modules/jobs/model/job-model.js
@@ -25,6 +25,7 @@ module.exports = function(sequelize, DataTypes){
         , ACTIVE        : {
               type      : DataTypes.BOOLEAN
             , allowNull : false
+            , defaultValue : false
         }
     }, {
         classMethods  : {
