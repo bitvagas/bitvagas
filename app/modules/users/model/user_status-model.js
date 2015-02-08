1263d450b58fb349de00e94ebebde15af56e7ce1
Cesar Augusto D. Azevedo <cesardeazevedo@outlook.com>
2015-02-07 23:32:43 -0200

    added user status account model

---
+++ app/modules/users/model/user_status-model.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes){
+
+    var user_status = sequelize.define('user_status', {
+        NAME : DataTypes.STRING
+      }, {
+          timestamps   : false
+        , tableName    : 'user_status'
+        , classMethods : {
+            associate  : function(models){
+                user_status.hasOne(models.user, {
+                    onDelete    : 'set null'
+                  , foreignKey  : {
+                      name      : 'USER_STATUS'
+                    , allowNull : false
+                    , defaultValue : 1
+                  }
+                });
+            }
+        }
+    });
+
+    return user_status;
+};
