62cef9f66dda43f08769c9313a82093c2012e531
Cesar Augusto D. Azevedo <cesardeazevedo@outlook.com>
2015-02-08 23:49:16 -0200

    added resolve to list categories

--- public/modules/jobs/app.js
+++ public/modules/jobs/app.js
@@ -3,6 +3,7 @@ angular.module('bitvagas.jobs',
     , 'bitvagas.jobs.controllers'
     , 'bitvagas.jobs.services'
     , 'bitvagas.jobs.category.services'
+    , 'bitvagas.jobs.organization.services'
     ])
     .config(function($urlRouterProvider, $stateProvider){
 
@@ -13,14 +14,19 @@ angular.module('bitvagas.jobs',
           , controller  : 'JobListController'
         })
         .state('jobs-list', {
-            url: 'jobs/list'
+            url: '/jobs/list'
           , templateUrl : '/modules/jobs/views/job-list'
           , controller  : 'JobListController'
         })
         .state('jobs-create', {
-            url: 'jobs/create'
-          , templateUrl : '/modules/jobs/views/job-create'
-          , controller  : 'JobCreateController'
+            url: '/jobs/create'
+          , templateUrl  : '/modules/jobs/views/job-create'
+          , resolve      : {
+              Categories : function(CategoryService){
+                  return CategoryService.findAll();
+              }
+          }
+          , controller   : 'JobCreateController'
         })
         .state('jobs-show', {
             url: '/jobs/:id'
