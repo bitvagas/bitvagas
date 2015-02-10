93e67a536867bff20bb4ef14a76df65a30d07f36
Cesar Augusto D. Azevedo <cesardeazevedo@outlook.com>
2015-02-09 23:56:28 -0200

    Implemented Verify account and invite user indirect functionality

--- app/modules/users/controllers/user-controller.js
+++ app/modules/users/controllers/user-controller.js
@@ -1,13 +1,13 @@
 var express  = require('express')
   , bcrypt   = require('bcryptjs')
+  , crypto   = require('crypto')
   , passport = require('passport')
   , db       = require('../../../models');
 
 module.exports = {
 
     findByEmail: function(request){
-        var email = request.param('EMAIL');
-        console.log('EMAIL: '+ email);
+        var email = request.body.EMAIL;
         return db.user.find({
             where   : { EMAIL : email }
           , include : [db.job]
@@ -25,23 +25,65 @@ module.exports = {
             else {
 
                 var salt = bcrypt.genSaltSync(4);
-                var hash = bcrypt.hashSync(request.body.PASSWORD || 'default', salt);
+                var hash = bcrypt.hashSync(request.body.PASSWORD, salt);
+                var hashEmail = bcrypt.hashSync(request.body.EMAIL, salt);
                 request.body.PASSWORD = hash;
+                request.body.TOKEN = hashEmail;
                 db.user.create(request.body).then(function(user){
 
                     user.PASSWORD = undefined;
+                    //TODO: Send a email to user.
+                    response.json(201, { data : user, token : hashEmail});
 
-                    request.logIn(user, function(err){
-
-                        if(err)
-                            response.json(400, err);
-                        else
-                            response.json(201, user);
-                    });
                 }).catch(function(err){
                     response.json(400, err);
                 });
             }
         });
+  }
+
+  , invite: function(request, response){
+      this.findByEmail(request).then(function(user){
+          if(user)
+                response.send(400, 'User already exists');
+          else{
+
+              crypto.randomBytes(20, function(err, bytes){
+                  var user = request.body;
+                  var token = bytes.toString('hex');
+                  user.RESETTOKEN   = token;
+                  user.RESETEXPIRES = Date.now() + 360000;
+                  user.PASSWORD = '';
+                  //User invited
+                  user.USER_STATUS = 1;
+                  db.user.create(user).then(function(user){
+                      response.json(201, user);
+                  }).catch(function(err){
+                      response.json(400, err);
+                  });
+              });
+          }
+      })
+  }
+
+  , verifyAccount: function(request, response){
+
+      var hashedEmail = request.body.token;
+      db.user.find({ where : { TOKEN : hashedEmail }})
+      .then(function(user){
+          return user.update({ USER_STATUS : 3 });
+      })
+      .then(function(user){
+
+          request.logIn(user, function(err){
+              if(err)
+                  response.json(400, err);
+              else
+                  response.redirect('/#/dashboard/overview');
+          });
+      })
+      .catch(function(err){
+          response.json(400, err);
+      });
   }
 }
--- app/modules/users/routes/user-router.js
+++ app/modules/users/routes/user-router.js
@@ -23,4 +23,30 @@ router.post('/api/signup', function(request, response){
     users.signup(request, response);
 });
 
+/*
+ * Signup an user indirect
+ */
+router.post('/invite', function(request, response){
+    users.invite(request, response);
+});
+
+router.get('/verify', function(request, response){
+    if(request.query.token === undefined)
+        return response.redirect('/#/signup');
+
+    response.render('verify', { token : request.query.token });
+});
+
+router.post('/verify', function(request, response){
+    users.verifyAccount(request, response);
+});
+
+router.get('/forgot', function(request, response){
+    response.render('forgot', { email : request.params.email || '' });
+});
+
+router.get('/reset', function(request, response){
+    response.render('reset');
+});
+
 module.exports = router;
