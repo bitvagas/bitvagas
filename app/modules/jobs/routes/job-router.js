e8dc0de963441db53d74b2e50b7e69fc5173521b
Cesar Augusto D. Azevedo <cesardeazevedo@outlook.com>
2015-02-19 23:56:32 -0200

    added post job router

--- app/modules/jobs/routes/job-router.js
+++ app/modules/jobs/routes/job-router.js
@@ -6,8 +6,16 @@ router.get('/', function(request, response){
     jobs.findAll(request,response);
 });
 
-router.post('/create', function(request, response){
+router.get('/:id', function(request, response){
+    jobs.findById(request, response);
+});
+
+router.post('/', function(request, response){
     jobs.create(request, response);
+});
+
+router.post('/post', function(request, response){
+    jobs.post(request, response);
 });
 
 module.exports = router;
