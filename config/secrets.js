6429dbcd2aab436f35d744a2be962590bb9a1327
Cesar Augusto D. Azevedo <cesardeazevedo@outlook.com>
2015-03-02 17:55:34 -0300

    added secret

--- config/secrets.js
+++ config/secrets.js
@@ -1,5 +1,5 @@
 module.exports = {
-    linkedIn  : {
+    linkedIn : {
         clientID          : process.env.LINKEDIN_ID
       , clientSecret      : process.env.LINKEDIN_SECRET
       , callbackURL       : process.env.LINKEDIN_CALLBACK_URL || "http://localhost:9000/auth/linkedin/callback"
