914f7645f9bccf638dfe6e8a0486a0ea6756f4b3
Cesar Augusto D. Azevedo <cesardeazevedo@outlook.com>
2015-03-01 23:37:20 -0300

    added secret linkedin api configuration file

---
+++ config/secrets.js
@@ -0,0 +1,9 @@
+module.exports = {
+    linkedIn  : {
+        clientID          : process.env.LINKEDIN_ID
+      , clientSecret      : process.env.LINKEDIN_SECRET
+      , callbackURL       : process.env.LINKEDIN_CALLBACK_URL || "http://localhost:9000/auth/linkedin/callback"
+      , scope             : ['r_fullprofile', 'r_emailaddress','r_network']
+      , passReqToCallback : true
+    }
+}
