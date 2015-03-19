6779354b8863e519e9fd40e247ba4ca345ef2180
César Augusto D. Azevedo <cesardeazevedo@outlook.com>
2015-03-18 23:49:51 -0300

    removed r_network linkedIn scope

--- config/secrets.js
+++ config/secrets.js
@@ -3,7 +3,7 @@ module.exports = {
         clientID          : process.env.LINKEDIN_ID
       , clientSecret      : process.env.LINKEDIN_SECRET
       , callbackURL       : process.env.LINKEDIN_CALLBACK_URL || "http://localhost:9000/auth/linkedin/callback"
-      , scope             : ['r_fullprofile', 'r_emailaddress','r_network']
+      , scope             : ['r_fullprofile', 'r_emailaddress']
       , passReqToCallback : true
     }
 }
