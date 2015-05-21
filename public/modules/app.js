2be7160a2d6c8644dbc92922427ff7e80e693994
Cesar Azevedo <cesardeazevedo@outlook.com>
2015-05-20 22:50:04 -0300

    Implemented angular translation for internacionalization
    translations 2%

--- public/modules/app.js
+++ public/modules/app.js
@@ -1,7 +1,18 @@
 angular.module('bitvagas',
-    ['bitvagas.main'
+    ['pascalprecht.translate'
+    ,'ngCookies'
+    ,'bitvagas.main'
     ,'bitvagas.jobs'
     ,'bitvagas.org'
     ,'bitvagas.users'
     ,'bitvagas.admin'
-    ]);
+    ]).config(function($translateProvider){
+        //Angular translation configuration
+        $translateProvider.useStaticFilesLoader({
+            prefix: 'locales/',
+            suffix: '.json'
+        })
+        .preferredLanguage('en')
+        .fallbackLanguage(['pt', 'en'])
+        .useLocalStorage();
+    });
