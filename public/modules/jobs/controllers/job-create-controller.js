8724877334039fa59da31f115d335d67bf0b31e7
Cesar Azevedo <cesardeazevedo@outlook.com>
2015-05-30 17:23:25 -0300

    Implemented dashboard create job transaltion
    * Removed unused information temporarily
    * General Transaltions 35%

--- public/modules/jobs/controllers/job-create-controller.js
+++ public/modules/jobs/controllers/job-create-controller.js
@@ -3,8 +3,8 @@ angular.module('bitvagas.jobs.controllers', ['pg-ng-dropdown'])
 .controller('JobCreateController', JobCreateController);
 
 JobPostController.$inject   = ['$scope', '$state', '$stateParams', 'JobService', 'Categories', 'ErrorHandling', 'lodash'];
-JobCreateController.$inject = ['$scope', '$state', '$stateParams', 'JobService', 'UserService', 'Categories', 'Organizations', 'lodash', 'ErrorHandling'];
-function JobPostController($scope, $state, $stateParams, JobService, Categories, ErrorHandling, _){
+JobCreateController.$inject = ['$scope', '$state', '$stateParams', 'JobService', 'Categories', 'Organizations', 'lodash', 'ErrorHandling'];
+function JobPostController($scope, $state, $stateParams, JobService, Categories,  ErrorHandling, _){
 
     $scope.data = $stateParams.data;
     $scope.errors = $stateParams.errors || [];
@@ -21,10 +21,8 @@ function JobPostController($scope, $state, $stateParams, JobService, Categories,
 
         JobService.post(data)
         .then(function(data){
-            console.log(data);
             $state.go('jobs-show', { 'id': data.data.id});
         }, function(err){
-            console.log(err);
             //back to crate state and show errors
             $state.go('jobs-create', {
                 data   : $scope.data
@@ -34,23 +32,19 @@ function JobPostController($scope, $state, $stateParams, JobService, Categories,
     };
 }
 
-function JobCreateController($scope, $state, $stateParams, JobService, UserService, Categories, Organizations, _, ErrorHandling){
+function JobCreateController($scope, $state, $stateParams, JobService, Categories, Organizations, _, ErrorHandling){
 
     $scope.data = $stateParams.data;
     $scope.errors = $stateParams.errors || [];
 
-    // $scope.categories = Categories.data;
     $scope.categories = _.dropdown(Categories.data, "NAME");
     $scope.orgs = _.dropdown(Organizations.data, "NAME");
-    $scope.orgs = Organizations.data;
 
     $scope.create = function(){
         $scope.data.CATEGORY_ID = _.selected($scope.categories, 'id');
         $scope.data.ORG_ID = _.selected($scope.orgs, 'id');
         if($scope.form.$valid)
             $state.go('dashboard.jobs.confirm', { data : $scope.data });
-        else
-            console.log($scope.form.$errors);
     };
 
     $scope.confirm = function(data){
@@ -59,7 +53,6 @@ function JobCreateController($scope, $state, $stateParams, JobService, UserServi
         .then(function(data){
             $state.go('dashboard.jobs.list');
         }, function(err){
-            console.log(err);
             //back to crate state and show errors
             $state.go('dashboard.jobs.create', {
                 data   : $scope.data
