8c1cb22a8bfec27a0b2d8391b0ae90cf08f1c9d6
Cesar Azevedo <cesardeazevedo@outlook.com>
2015-07-07 08:34:13 -0300

    Move errors to json and error page for production

--- config/express.js
+++ config/express.js
@@ -12,60 +12,51 @@ var express        = require('express')
 
 module.exports = function(app, config) {
 
-  app.set('views', './app/views');
-  app.set('view engine', 'jade');
+    app.set('views', './app/views');
+    app.set('view engine', 'jade');
 
-  app.use(favicon(config.root + '/public/img/favicon.ico'));
-  app.use(logger('dev'));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-  app.use(cookieParser());
-  app.use(compress());
-  app.use(express.static(config.root + '/public'));
-  app.use(methodOverride());
-  app.use(passport.initialize());
+    app.use(favicon(config.root + '/public/img/favicon.ico'));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(cookieParser());
+    app.use(compress());
+    app.use(express.static(config.root + '/public'));
+    app.use(methodOverride());
+    app.use(passport.initialize());
 
-  //Initialize HarpJS
-  app.use(harp.mount(config.root + "/public"));
+    //Initialize HarpJS
+    app.use(harp.mount(config.root + "/public"));
 
-  var api    = glob.sync(config.root + "/app/modules/**/api/*.js");
-  var routes = glob.sync(config.root + "/app/modules/**/routes/*.js");
-  routes.forEach(function(route){
-      require(route)(app);
-  });
+    var api    = glob.sync(config.root + "/app/modules/**/api/*.js");
+    var routes = glob.sync(config.root + "/app/modules/**/routes/*.js");
+    routes.forEach(function(route){
+        require(route)(app);
+    });
 
-  api.forEach(function(route){
-      //get api name prefix
-      app.use('/api', require(route)(app));
-  });
+    api.forEach(function(route){
+        //get api name prefix
+        app.use('/api', require(route)(app));
+    });
 
-  app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
+    app.use(function (req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
 
-  // error handlers
-  if(app.get('env') === 'development'){
+    // error handlers
+    if(app.get('env') === 'development'){
+        app.use(function (err, req, res, next) {
+            res.status(err.status || 500);
+            res.json({ message: err.message });
+        });
+    }
+
     app.use(function (err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: err,
-        title: 'error'
-      });
+        res.status(err.status || 500);
+        res.redirect('/');
     });
-  }
-
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: {},
-      title: 'error'
-    });
-  });
-
 };
