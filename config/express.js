6ca7fa1f69c11eb7f7335c6b017c8161017e338f
cesardeazevedo <cesardeazevedo@outlook.com>
2016-04-10 19:09:30 -0300

    Added prerender integration

--- config/express.js
+++ config/express.js
@@ -29,6 +29,7 @@ module.exports = function(app, config) {
     app.use(methodOverride());
     app.use(passport.initialize());
     app.disable('x-powered-by');
+    app.use(require('prerender-node').set('prerenderToken', process.env.PRERENDER));
 
     //Initialize HarpJS
     app.use(harp.mount(config.root + "/public"));
